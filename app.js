abb228a3dc09cf22c9e50e9ab854f3919cead70e
UserGitHub37 <bum37@mail.ru>
2022-09-05 22:11:12 +0300

    feat: add eslint and bcryptjs

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+mongoose.connect('mongodb://localhost:27017/moviesdb');
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
