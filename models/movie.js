3a33c972f4b4869a7a0a82c422fc7aaa3c99df90
UserGitHub37 <bum37@mail.ru>
2022-09-05 23:18:06 +0300

    chore: create movie schema

---
+++ models/movie.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+
+const { urlRegex } = require('../utils/config');
+
+const cardSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return urlRegex.test(v);
+      },
+      message: 'Не валидный URL',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return urlRegex.test(v);
+      },
+      message: 'Не валидный URL',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return urlRegex.test(v);
+      },
+      message: 'Не валидный URL',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('card', cardSchema);
---
+++ utils/config.js
@@ -0,0 +1,5 @@
+const urlRegex = /^https?:\/\/(w{3}\.)?[0-9a-z.-]{1,256}(\/([0-9a-z\-._~:/?#[\]@!$&'()*+,;=])*)?$/i;
+
+module.exports = {
+  urlRegex,
+};
