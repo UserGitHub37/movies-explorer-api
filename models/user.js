242bfd7e2107e609bce6377ab38f5930e672e1ff
UserGitHub37 <bum37@mail.ru>
2022-09-05 21:50:30 +0300

    chore: create user schema

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcryptjs');
+const Unauthorized = require('../errors/unauthorized-err');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    default: 'Андрей',
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return isEmail(v);
+      },
+      message: 'Не валидный адрес электронной почты',
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+}, { versionKey: false });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Unauthorized('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Unauthorized('Неправильные почта или пароль'));
+          }
+
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
