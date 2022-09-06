5d59ccd289839bfeb826c81257c7eb5ca78b8053
UserGitHub37 <bum37@mail.ru>
2022-09-06 15:10:13 +0300

    chore: create routes and controllers for users

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
+const NotFoundError = require('./errors/not-found-err');
 
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/moviesdb');
+
+app.use('/users', require('./routes/users'));
+
+app.use('/movies', require('./routes/movies'));
+
+app.use('*', (req, res, next) => next(new NotFoundError('404 Not Found')));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,24 @@
+const {
+  NODE_ENV,
+  SALT_ROUND = 10,
+  JWT_SECRET = 'some-secret-key',
+  ALLOWED_CORS,
+} = process.env;
+
 const urlRegex = /^https?:\/\/(w{3}\.)?[0-9a-z.-]{1,256}(\/([0-9a-z\-._~:/?#[\]@!$&'()*+,;=])*)?$/i;
 
+const allowedCors = NODE_ENV !== 'production'
+  ? [
+    'http://localhost:3000',
+    'https://localhost:3000',
+    'http://localhost:3001',
+    'https://localhost:3001',
+  ]
+  : ALLOWED_CORS.split(', ');
+
 module.exports = {
+  SALT_ROUND: Number(SALT_ROUND),
+  JWT_SECRET,
   urlRegex,
+  allowedCors,
 };
